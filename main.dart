e8ce4901c67792c341bb412bc39af29f8071c3a5
YuDavidCao <yu.cao20041208@gmail.com>
2023-12-07 22:01:41 -0500

    initial commit

---
+++ main.dart
@@ -0,0 +1,44 @@
+import 'route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'pages.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.top]);  
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusManager.instance.primaryFocus?.unfocus();
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Title',
+        theme: ThemeData(
+          primarySwatch: const MaterialColor(0xFF0E2C71, {
+            50: Color(0xFF0E2C71),
+            100: Color(0xFF0E2C71),
+            200: Color(0xFF0E2C71),
+            300: Color(0xFF0E2C71),
+            400: Color(0xFF0E2C71),
+            500: Color(0xFF0E2C71),
+            600: Color(0xFF0E2C71),
+            700: Color(0xFF0E2C71),
+            800: Color(0xFF0E2C71),
+            900: Color(0xFF0E2C71),
+          }),
+        ),
+          home: const HomePage(),
+          onGenerateRoute: RouteGenerator.generateRoute,
+      ),
+    );
+  }
+}
